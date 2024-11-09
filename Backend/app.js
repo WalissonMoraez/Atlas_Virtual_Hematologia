ce4dac6cdab6c5b243e5fabc2f06cf9dd5c577b5
WalissonMoraez <walissonmoraes15@gmail.com>
2024-11-09 19:15:15 -0300

    frontend esta puxando corretamente as categorias para a pagina principal

--- Backend/app.js
+++ Backend/app.js
@@ -5,7 +5,9 @@ const Atlas_routes = require('./routes/Atlas_routes');
 const Categoria_routes = require('./routes/Categoria_routes');
 const Postagem_routes = require('./routes/Postagem_routes');
 const Quiz_routes = require('./routes/Quiz_routes');
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 
 // Aplicação dos arquivos de rotas
@@ -13,6 +15,7 @@ app.use('/api/atlas', Atlas_routes);        // Rotas principais do atlas
 app.use('/api/atlas', Categoria_routes);     // Rotas específicas para categorias
 app.use('/api/atlas', Postagem_routes);      // Rotas específicas para postagens
 app.use('/api/quiz', Quiz_routes);          // Rotas específicas para quizzes
+app.use(cors({ origin: 'http://localhost:4000' })); //Essa configuração permitirá que qualquer origem acesse a API. Para limitar a origens específicas
 
 const PORT = 3000;
 app.listen(PORT, () => {
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,23 +1,44 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000', // Substitua pela URL do seu backend
+    baseURL: 'http://localhost:3000/api', // Substitua pela URL do seu backend
 });
 
 export const getCategories = async () => {
     try {
-        const response = await api.get('/categories');
+        const response = await api.get('/atlas/categorias');
         return response.data;
     } catch (error) {
         console.error('Erro ao buscar categorias:', error);
     }
 };
 
-export const getCategoryDetails = async (id) => {
+// Função para buscar postagens de uma categoria específica
+export const getCategoryDetails = async (categoriaId) => {
     try {
-        const response = await api.get(`/categories/${id}`);
+        const response = await api.get(`/atlas/categorias/${categoriaId}/posts`);
         return response.data;
     } catch (error) {
         console.error('Erro ao buscar detalhes da categoria:', error);
+    }
+};
+
+// Função para obter uma postagem específica
+export const getPostDetails = async (categoriaId, postId) => {
+    try {
+        const response = await api.get(`/atlas/categoria/${categoriaId}/post/${postId}`);
+        return response.data;
+    } catch (error) {
+        console.error('Erro ao buscar detalhes da postagem:', error);
+    }
+};
+
+// Função para buscar um quiz específico
+export const getQuiz = async (quizId) => {
+    try {
+        const response = await api.get(`/quiz/${quizId}`);
+        return response.data;
+    } catch (error) {
+        console.error('Erro ao buscar quiz:', error);
     }
 };
