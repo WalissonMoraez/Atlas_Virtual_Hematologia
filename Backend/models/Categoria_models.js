58c704e687151688b47c209c41c7ff3603a5caa7
WalissonMoraez <walissonmoraes15@gmail.com>
2024-11-18 15:57:31 -0300

    retirei bugs

--- Backend/models/Categoria_models.js
+++ Backend/models/Categoria_models.js
@@ -1,15 +1,15 @@
 class Categoria_models {
-    constructor(id, name, description) {
-      this.id = id;
-      this.name = name;
-      this.description = description;
-      this.posts = [];
-    }
+  constructor(id, name, description) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.posts = [];
+  }
 
-    addPost(post) {
-      this.posts.push(post);
-    }
-    
+  addPost(post) {
+    this.posts.push(post);
   }
   
+}
+
 module.exports = Categoria_models;
