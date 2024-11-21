6ad6e7cd9244803803d9f9d84f7b92f1199ddeb3
WalissonMoraez <walissonmoraes15@gmail.com>
2024-11-21 15:40:13 -0300

    Dia 21/11 tentando resolver front - imagens

--- frontend/src/pages/PostDetailPage.js
+++ frontend/src/pages/PostDetailPage.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { BASE_URL } from '../config'
 import Navbar2 from '../components/Navbar2';
 import Footer from '../components/Footer';
 import { getPostDetails } from '../services/api';
